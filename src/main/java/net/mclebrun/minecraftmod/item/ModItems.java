bdf74287c8a2ed9c5738e67d74cd8e0105bbe521
Mike-at-TRU <lebrunm231@mytru.ca>
2024-10-05 20:56:12 -0700

    added magic block

--- src/main/java/net/mclebrun/minecraftmod/item/ModItems.java
+++ src/main/java/net/mclebrun/minecraftmod/item/ModItems.java
@@ -1,2 +1,23 @@
-package net.mclebrun.minecraftmod.item;public class ModItems {
+package net.mclebrun.minecraftmod.item;
+
+import net.mclebrun.minecraftmod.MinecraftMod;
+import net.mclebrun.minecraftmod.item.custom.Chiseltem;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MinecraftMod.MOD_ID);
+
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
+            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
+            () -> new Chiseltem(new Item.Properties().durability(32)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
--- src/main/java/net/mclebrun/minecraftmod/item/custom/Chiseltem.java
+++ src/main/java/net/mclebrun/minecraftmod/item/custom/Chiseltem.java
@@ -1,2 +1,68 @@
-package net.mclebrun.minecraftmod.item.custom;public class Chiseltem {
+package net.mclebrun.minecraftmod.item.custom;
+
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class Chiseltem extends Item {
+    private static final Map<Block, Block> CHISEL_MAP =
+            Map.of(
+                    Blocks.STONE, Blocks.STONE_BRICKS,
+                    Blocks.CRACKED_DEEPSLATE_TILES, Blocks.CRACKED_DEEPSLATE_BRICKS,
+                    Blocks.END_STONE, Blocks.END_STONE_BRICKS,
+                    Blocks.DEEPSLATE, Blocks.DEEPSLATE_BRICKS,
+                    Blocks.NETHERRACK, Blocks.NETHER_BRICKS
+            );
+
+    private boolean didWork = false;
+
+    public Chiseltem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        Block clickedBlock = level.getBlockState(context.getClickedPos()).getBlock();
+        if (!level.isClientSide()) {
+            if (CHISEL_MAP.containsKey(clickedBlock)) {
+                level.setBlockAndUpdate(context.getClickedPos(), CHISEL_MAP.get(clickedBlock).defaultBlockState());
+                didWork = true;
+
+            } else if (CHISEL_MAP.containsValue(clickedBlock)) {
+
+                level.setBlockAndUpdate(context.getClickedPos(), getKeyByValue(CHISEL_MAP, clickedBlock).defaultBlockState());
+
+            }
+
+            if (didWork) {
+                didWork = false;
+                context.getItemInHand().hurtAndBreak(1, ((ServerLevel) level), context.getPlayer(),
+                        item -> context.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
+                level.playSound(null, context.getClickedPos(), SoundEvents.GRINDSTONE_USE, SoundSource.BLOCKS);
+            }
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    private Block getKeyByValue(Map<Block, Block> map, Block clickedBlock) {
+        for (Map.Entry<Block, Block> entry : map.entrySet()) {
+            if (Objects.equals(clickedBlock, entry.getValue())) {
+                didWork = true;
+                return entry.getKey();
+            }
+        }
+        return clickedBlock;
+    }
 }

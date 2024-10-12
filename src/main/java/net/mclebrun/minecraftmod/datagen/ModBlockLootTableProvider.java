9c4ea9f682ce4879eb6bdb4c7f9d658a66843c08
Mike-at-TRU <lebrunm231@mytru.ca>
2024-10-12 03:41:09 -0700

    data gen!!

---
+++ src/main/java/net/mclebrun/minecraftmod/datagen/ModBlockLootTableProvider.java
@@ -0,0 +1,52 @@
+package net.mclebrun.minecraftmod.datagen;
+
+import net.mclebrun.minecraftmod.block.ModBlocks;
+import net.mclebrun.minecraftmod.item.ModItems;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+
+import java.util.Set;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider {
+    protected ModBlockLootTableProvider(HolderLookup.Provider registries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+    }
+
+    @Override
+    protected void generate() {
+        dropSelf(ModBlocks.BISMUTH_BLOCK.get());
+        dropSelf(ModBlocks.MAGIC_BLOCK.get());
+        add(ModBlocks.BISMUTH_ORE.get(),
+                block -> createMultipleOreDrops(ModBlocks.BISMUTH_BLOCK.get(), ModItems.RAW_BISMUTH.get(), 1f, 1f));
+        add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get(),
+                block -> createMultipleOreDrops(ModBlocks.BISMUTH_DEEPSLATE_ORE.get(), ModItems.RAW_BISMUTH.get(), 2f, 5f));
+
+    }
+
+    @Override
+    //because this is all the blocks all will need a loot table
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    }
+
+    protected LootTable.Builder createMultipleOreDrops(Block pBlock, Item itemToBeDroppedWithoutSilkTouch, float minimumNumberOfItemsToBeDropped, float maximumNumberOfItemsToBeDropped) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(pBlock,
+                this.applyExplosionDecay(pBlock, LootItem.lootTableItem(itemToBeDroppedWithoutSilkTouch)
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(minimumNumberOfItemsToBeDropped, maximumNumberOfItemsToBeDropped)))
+                        .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))));
+    }
+}
---
+++ src/main/java/net/mclebrun/minecraftmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,34 @@
+package net.mclebrun.minecraftmod.datagen;
+
+import net.mclebrun.minecraftmod.MinecraftMod;
+import net.mclebrun.minecraftmod.item.ModItems;
+import net.mclebrun.minecraftmod.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends ItemTagsProvider {
+
+
+    public ModItemTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockTags, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, blockTags, MinecraftMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+
+        tag(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.BISMUTH.get())
+                .add(ModItems.RAW_BISMUTH.get())
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.COMPASS);
+
+    }
+}
